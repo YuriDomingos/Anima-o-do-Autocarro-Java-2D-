464728a09cbf1ac110a586975c2a877431a2f238
Yuri Domingos <64822018+YuriDomingos@users.noreply.github.com>
2020-12-20 07:42:38 +0100

    first sectioon

---
+++ pessoa.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carwalkingcg;
+
+/**
+ *
+ * @author yuri
+ */
+public class pessoa {
+    
+    private int coordX, coordY, largura, altura;
+    private int head, body, armleft, armright, legLeft, legRight;
+    
+    
+
+    public int getCoordX() {
+        return coordX;
+    }
+
+    public void setCoordX(int coordX) {
+        this.coordX = coordX;
+    }
+
+    public int getCoordY() {
+        return coordY;
+    }
+
+    public void setCoordY(int coordY) {
+        this.coordY = coordY;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getHead() {
+        return head;
+    }
+
+    public void setHead(int head) {
+        this.head = head;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+
+    public int getArmleft() {
+        return armleft;
+    }
+
+    public void setArmleft(int armleft) {
+        this.armleft = armleft;
+    }
+
+    public int getArmright() {
+        return armright;
+    }
+
+    public void setArmright(int armright) {
+        this.armright = armright;
+    }
+
+    public int getLegLeft() {
+        return legLeft;
+    }
+
+    public void setLegLeft(int legLeft) {
+        this.legLeft = legLeft;
+    }
+
+    public int getLegRight() {
+        return legRight;
+    }
+
+    public void setLegRight(int legRight) {
+        this.legRight = legRight;
+    }
+    
+    
+    
+}
